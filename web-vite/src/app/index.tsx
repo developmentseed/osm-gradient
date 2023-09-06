29208f5290699f87b69af0d634f43c486977fa6e
Vitor George <vitor.george@gmail.com>
2023-09-06 13:28:52 +0100

    Add loading state

--- web-vite/src/app/index.tsx
+++ web-vite/src/app/index.tsx
@@ -1,15 +1,20 @@
 import "./index.css";
 import { Map } from "./map";
-import { useAppReducer } from "./reducer";
+import { MapStatus, useAppReducer } from "./reducer";
 import { Stats } from "./stats";
 
 export function App() {
   const [appState, dispatchAppState] = useAppReducer();
 
+  const { mapStatus, stats } = appState;
+
   return (
     <>
       <Map appState={appState} dispatchAppState={dispatchAppState} />
-      {appState?.stats && <Stats stats={appState.stats} />}
+      {mapStatus === MapStatus.READY && stats && (
+        <Stats stats={appState.stats} />
+      )}
+      {mapStatus === MapStatus.LOADING && <div>Loading...</div>}
     </>
   );
 }
