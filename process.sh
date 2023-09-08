965326914a3659a63c0db7882cc8d71422974f10
Tarashish Mishra <sunu@sunu.in>
2023-09-08 14:39:48 +0100

    update the processing script to take a date and an hour as input

--- process.sh
+++ process.sh
@@ -2,18 +2,38 @@
 
 # Check if the necessary arguments were provided
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
-    echo "Usage: $0 <start_number> <num_files>"
+    echo "Usage: $0 <date> <hour> [num_files]"
     exit 1
 fi
 
 # Starting number provided as the first argument
-start_num=$1
+start_date=$1
+
+start_hour=$2
+
+# Add a leading zero if the hour is less than 10
+[[ $start_hour -lt 10 && $start_hour -ge 0 ]] && start_hour="0$start_hour"
 
 # Number of files to fetch
-num_files=$2
+num_files=${3:-60}
+if (( num_files < 1 )); then
+    num_files=1
+fi
+if (( num_files > 60 )); then
+    num_files=60
+fi
 
-# Calculate the end number
-end_num=$((start_num - num_files))
+echo "Number of files to fetch: ${num_files}"
+
+start_num=$(( $(date -d "${start_date}T${start_hour}:00:00Z" +%s)/60 - 22457216))
+
+end_num=$((start_num + num_files - 1))
+
+if [[ $start_num != $end_num ]]; then
+    echo "Fetching files from number ${start_num} to ${end_num}"
+else
+    echo "Fetching file number ${start_num}"
+fi
 
 # Fetch .osc files using fetchOsc.js
 fetchOscFunction() {
@@ -24,21 +44,22 @@ fetchOscFunction() {
 
 export -f fetchOscFunction
 
-seq $end_num $start_num | parallel fetchOscFunction
+seq $start_num $end_num | parallel fetchOscFunction
 
 # Parse .osc files and collect into a single geojson using parser.js and jq
 echo 'Parsing files'
-for (( i=start_num; i>end_num; i-- )); do
+for (( i=start_num; i<end_num; i++ )); do
     echo $i
     file_path="/tmp/${i}.osc"
-    node parser.js $file_path >> "/tmp/${i}.geojson"
+    node parser.js $file_path > "/tmp/${start_date}-${start_hour}-${i}.geojson"
 done
 echo 'Done parsing files'
 
-echo 'Combining files'
-geojson-merge /tmp/*.geojson > /tmp/final_geojson.json
-echo 'Done combining files'
+echo "Combining files"
+output_file="/tmp/${start_date}T${start_hour}:00.geojson"
+geojson-merge "/tmp/${start_date}-${start_hour}-*.geojson" > $output_file
+echo "Done combining files"
 
 echo 'Replace nulls with 0'
-sed -i 's/null/0/g' /tmp/final_geojson.json
+sed -i 's/null/0/g' $output_file
 echo 'Done replacing nulls with 0'
