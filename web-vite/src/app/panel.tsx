ff63d58cf5df9ffd6b3c63f639c314a41e0cf483
Vitor George <vitor.george@gmail.com>
2023-09-14 16:40:48 +0100

    Lint

--- web-vite/src/app/panel.tsx
+++ web-vite/src/app/panel.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
+// TODO - Fix accessibility issues in this file and remove the following
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 interface PanelProps {
   children: any;
 }
-export function Panel(props: PanelProps){
-  const [openPanel, setOpenPanel] = useState(false)
+export function Panel(props: PanelProps) {
+  const [openPanel, setOpenPanel] = useState(false);
   return (
-    <div  class={openPanel ? "panel open" : "panel"}>
-      <span onClick={() => setOpenPanel(!openPanel)} class="panel__dragger">-</span>
+    <div class={openPanel ? "panel open" : "panel"}>
+      <span onClick={() => setOpenPanel(!openPanel)} class="panel__dragger">
+        -
+      </span>
       {props.children}
     </div>
-  )
+  );
 }
