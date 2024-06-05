fef5a90e9fdaa1fe152c143760a517c953529ed8
Vitor George <vitor.george@gmail.com>
2024-06-05 17:09:31 +0100

    Add text copy

--- packages/web/src/app/inputs.tsx
+++ packages/web/src/app/inputs.tsx
@@ -8,6 +8,15 @@ export function PanelInputs(props: PanelInputsProps) {
   return (
     <article>
       <section>
+        <small>
+          <div>
+            Visualize OpenStreetMap features created, modified or deleted every
+            hour using change data prepared by OSMCha as a FlatGeobuf. This
+            visualization does not use a server.
+          </div>
+        </small>
+      </section>
+      <section>
         <h3>Area of Interest (AOI)</h3>
         <small>Zoom and pan the map to set the AOI for stats </small>
         <p>
